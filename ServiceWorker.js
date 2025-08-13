625a43256e4dd604843a72f8da3a129a294a501e
Muhammad Saad Farooq <saad.farooq@tecshield.io>
2025-08-13 16:17:10 +0500

    files added

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Web Cube AR-0.1";
+const contentToCache = [
+    "Build/Web AR U6 Video.loader.js",
+    "Build/Web AR U6 Video.framework.js",
+    "Build/Web AR U6 Video.data",
+    "Build/Web AR U6 Video.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
